e19754ff0f0ac1837bd2b3d639f8cb2cb503d6fc
silvia <smidori@gmail.com>
2015-11-26 21:39:52 -0200

    alurapic

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,31 @@
+angular.module('Alurapic').controller('FotosController', function($scope, $http){
+    $scope.fotos = [];
+    
+    $http.get("v1/fotos").success(
+        function(fotos){
+            $scope.fotos = fotos;
+        }
+    ).error(function(error){
+        console.log(error);
+    })
+    
+    
+    /*
+       $scope.fotos = [
+        {
+        url : "http://www.fundosanimais.com/Minis/leoes.jpg", 
+        titulo : "Leão 1"
+        }, 
+        {
+        url : "http://www.fundosanimais.com/Minis/leoes.jpg", 
+        titulo : "Leão 2"
+        },
+        {
+        url : "http://www.fundosanimais.com/Minis/leoes.jpg", 
+        titulo : "Leão 3"
+        }
+        ] 
+    */
+    
+    
+}); 
